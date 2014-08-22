74965901ffe253b5ac62e5e46d5d5fb2957de28f
Mattt Thompson <m@mattt.me>
2014-08-22 09:21:18 -0700

    Initial Commit

---
+++ Literally.swift
@@ -0,0 +1,175 @@
+// Literally.swift
+//
+// Copyright (c) 2014 Mattt Thompson (http://mattt.me)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import Foundation
+
+// MARK: NSCharacterSet
+
+extension NSCharacterSet: ArrayLiteralConvertible {
+    public class func convertFromArrayLiteral(characters: Character...) -> Self {
+        return self(charactersInString: join("", characters.map({String($0)})))
+    }
+}
+
+extension NSCharacterSet: StringLiteralConvertible {
+    typealias ExtendedGraphemeClusterLiteralType = StringLiteralType
+
+    public class func convertFromExtendedGraphemeClusterLiteral(value: StringLiteralType) -> Self {
+        return self(charactersInString: value)
+    }
+
+    public class func convertFromStringLiteral(value: StringLiteralType) -> Self {
+        return self(charactersInString: value)
+    }
+}
+
+// MARK: NSExpression
+
+extension NSExpression: StringLiteralConvertible {
+    typealias ExtendedGraphemeClusterLiteralType = StringLiteralType
+
+    public class func convertFromExtendedGraphemeClusterLiteral(value: StringLiteralType) -> Self {
+        return self(format: value, argumentArray: [])
+    }
+
+    public class func convertFromStringLiteral(value: StringLiteralType) -> Self {
+        return self(format: value, argumentArray: [])
+    }
+}
+
+// MARK: NSIndexPath
+
+extension NSIndexPath: ArrayLiteralConvertible {
+    public class func convertFromArrayLiteral(indexes: Int...) -> Self {
+        return self(indexes: indexes, length: indexes.count)
+    }
+}
+
+// MARK: NSIndexSet
+
+extension NSIndexSet: ArrayLiteralConvertible {
+    public class func convertFromArrayLiteral(indexes: Int...) -> Self {
+        var mutableIndexSet = NSMutableIndexSet()
+        for index in indexes {
+            mutableIndexSet.addIndex(index)
+        }
+
+        return self(indexSet: mutableIndexSet)
+    }
+}
+
+// MARK: NSNull
+
+extension NSNull: NilLiteralConvertible {
+    public class func convertFromNilLiteral() -> Self {
+        return self()
+    }
+}
+
+// MARK: NSOrderedSet
+
+extension NSOrderedSet: ArrayLiteralConvertible {
+    public class func convertFromArrayLiteral(elements: AnyObject...) -> Self {
+        return self(array: elements)
+    }
+}
+
+let orderedSet: NSOrderedSet = ["a", "b", "c"]
+orderedSet.array
+
+// MARK: NSPredicate
+
+extension NSPredicate: StringLiteralConvertible {
+    typealias ExtendedGraphemeClusterLiteralType = StringLiteralType
+
+    public class func convertFromExtendedGraphemeClusterLiteral(value: StringLiteralType) -> Self {
+        return self(format: value, argumentArray: [])
+    }
+
+    public class func convertFromStringLiteral(value: StringLiteralType) -> Self {
+        return self(format: value, argumentArray: [])
+    }
+}
+
+// MARK: NSRegularExpression
+
+extension NSRegularExpression: StringLiteralConvertible {
+    typealias ExtendedGraphemeClusterLiteralType = StringLiteralType
+
+    public class func convertFromExtendedGraphemeClusterLiteral(value: StringLiteralType) -> Self {
+        return self(pattern: value, options: nil, error: nil)
+    }
+
+    public class func convertFromStringLiteral(value: StringLiteralType) -> Self {
+        return self(pattern: value, options: nil, error: nil)
+    }
+}
+
+// MARK: NSScanner
+
+extension NSScanner: StringLiteralConvertible {
+    typealias ExtendedGraphemeClusterLiteralType = StringLiteralType
+
+    public class func convertFromExtendedGraphemeClusterLiteral(value: StringLiteralType) -> Self {
+        return self(string: value)
+    }
+
+    public class func convertFromStringLiteral(value: StringLiteralType) -> Self {
+        return self(string: value)
+    }
+}
+
+// MARK: NSSet
+
+extension NSSet: ArrayLiteralConvertible {
+    public class func convertFromArrayLiteral(elements: AnyObject...) -> Self {
+        return self(array: elements)
+    }
+}
+
+// MARK: NSTimeZone
+
+extension NSTimeZone: StringLiteralConvertible {
+    typealias ExtendedGraphemeClusterLiteralType = StringLiteralType
+
+    public class func convertFromExtendedGraphemeClusterLiteral(value: StringLiteralType) -> Self {
+        return self(name: value)
+    }
+
+    public class func convertFromStringLiteral(value: StringLiteralType) -> Self {
+        return self(name: value)
+    }
+}
+
+// MARK: NSURL
+
+extension NSURL: StringLiteralConvertible {
+    typealias ExtendedGraphemeClusterLiteralType = StringLiteralType
+
+    public class func convertFromExtendedGraphemeClusterLiteral(value: StringLiteralType) -> Self {
+        return self(string: value)
+    }
+
+    public class func convertFromStringLiteral(value: StringLiteralType) -> Self {
+        return self(string: value)
+    }
+}
---
+++ README.md
@@ -0,0 +1,141 @@
+# Literally
+*Swift Literal Convertibles for Foundation*
+
+In Swift, literals can be used to provide convenient shorthand initializers for any object conforming to the corresponding protocol. For example, an `NSURL` can be initialized from a string literal (`let url: NSURL = "http://example.com"`) if `NSURL` is extended to conform to the `StringLiteralConvertible` protocol (which implements `convertFromStringLiteral`).
+
+Literally is a demonstration of how Foundation framework classes can be extended to take advantage of this language feature. Best efforts have been made to find useful opportunities, while forgoing ones that could technically work, but would not yield any benefit (e.g. conforming `NSMapTable` to `DictionaryLiteralConvertible`, since it would not be able to specify storage options). **This is not recommended for usage in production code.** In time, it is expected that Cocoa and the Swift standard library will coalesce in significant ways, making this kind of adaptation irrelevant.
+
+> For more information about literal convertibles in Swift, check out [this NSHipster article](http://nshipster.com/swift-literal-convertible/).
+
+## Installation
+
+_The infrastructure and best practices for distributing Swift libraries is currently being developed by the developer community during this beta period of the language and Xcode. In the meantime, you can simply add Literally as a git submodule, and drag the `Literally.swift` file into your Xcode project._
+
+---
+
+## Inventory
+
+### ArrayLiteralConvertible
+
+- `NSCharacterSet`
+- `NSCountedSet` (subclass of `NSMutableSet`)
+- `NSIndexPath`
+- `NSIndexSet`
+- `NSOrderedSet`
+- `NSSet`
+
+
+### NilLiteralConvertible
+
+- `NSNull`
+
+### StringLiteralConvertible
+
+- `NSCharacterSet`
+- `NSExpression`
+- `NSPredicate`
+- `NSRegularExpression`
+- `NSScanner`
+- `NSTimeZone`
+- `NSURL`
+
+## Usage
+
+### NSCharacterSet
+
+```swift
+let characterSetFromArray: NSCharacterSet = ["$", "€", "¥", "£"]
+characterSetFromArray.bitmapRepresentation
+
+let characterSetFromString: NSCharacterSet = "aeiouy"
+characterSetFromString.bitmapRepresentation
+```
+
+### NSCountedSet
+
+```swift
+let countedSet: NSCountedSet = [1, 2, 2, 3, 3, 3]
+countedSet.countForObject(2)
+```
+
+### NSExpression
+
+```swift
+let expression: NSExpression = "4 + 4"
+expression.operand
+```
+
+### NSIndexPath
+
+```swift
+let indexPath: NSIndexPath = [3,17]
+indexPath.indexAtPosition(1)
+```
+
+### NSIndexSet
+
+```swift
+let indexSet: NSIndexSet = [1, 2, 3, 4, 5]
+indexSet.containsIndex(4)
+```
+
+### NSNull
+
+```swift
+let null: NSNull = nil
+```
+
+### NSPredicate
+
+```swift
+let predicate = "value <> 'foo'"
+```
+
+### NSRegularExpression
+
+```swift
+let regex: NSRegularExpression = "foo"
+regex.numberOfMatchesInString("foobar", options: nil, range: NSMakeRange(0, 6))
+```
+
+### NSScanner
+
+```swift
+let scanner: NSScanner = "foo: 1, bar: 2, baz: 3"
+var foo: NSString?
+scanner.scanUpToString(":", intoString: &foo)
+foo!
+```
+
+### NSSet
+
+```swift
+let set: NSSet = [1, 2, 3]
+set.containsObject(3)
+```
+
+### NSTimeZone
+
+```swift
+let timeZone: NSTimeZone = "America/Los_Angeles"
+timeZone.secondsFromGMTForDate(NSDate())
+```
+
+### NSURL
+
+let url: NSURL = "http://github.com/mattt/Literally"
+url.host!
+
+---
+
+## Contact
+
+Mattt Thompson
+
+- http://github.com/mattt
+- http://twitter.com/mattt
+- m@mattt.me
+
+## License
+
+Literally is available under the MIT license. See the LICENSE file for more info.
